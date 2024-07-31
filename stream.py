bde9990c74ebc30a5746a86769f136d5475783c6
Analyst-FPnA <analyst.fpna@gmail.com>
2024-07-31 15:52:43 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -5,17 +5,17 @@ import io
 import pandas as pd
 import os
 
-# Fungsi untuk mendownload file ZIP dari Google Drive
+# Let's spoof a common user-agent (e.g. Chrome 74 / Windows 10).
+# Doing so will fool Apache into thinking that we're making a request
+# via the Chrome web browser.
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+
 url = 'https://drive.google.com/uc?export=download&id=1f-YfvMvFG0UaOw9H5RqNpkPRJEB-8gfa'
-response = requests.get(url)
-response.raise_for_status()
+request = requests.get(url, stream=True, headers=headers)
 
-# Menyimpan file untuk pemeriksaan manual
-with open('downloaded_file.zip', 'wb') as file:
-    file.write(response.content)       
-st.write(os.listdir())
-
-with zipfile.ZipFile('downloaded_file.zip', 'r') as zip_ref:
-    zip_ref.extractall()
-
+# Use the url to determine the filename to save the data as.
+# Finally, write out the streamed data as binary data.
+zip_filename = os.path.basename(url)
+with open(zip_filename, 'wb') as zfile:
+    zfile.write(request.content)
 st.write(os.listdir())
