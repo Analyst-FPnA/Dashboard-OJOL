441d8b5e1a74cbf7c1dc5fcfef7a15c7225f7882
Analyst-FPnA <analyst.fpna@gmail.com>
2024-07-31 16:07:16 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -4,13 +4,17 @@ import zipfile
 import io
 import pandas as pd
 import os
+import gdown
 
-from google_drive_downloader import GoogleDriveDownloader as gdd
+def download_file_from_google_drive(file_id, dest_path):
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, dest_path, quiet=False)
 
-gdd.download_file_from_google_drive(file_id='1wMeJXGaFF1ku2-txWshzDLaHoxS_tBz0',
-                                    dest_path='doqwnload.zip',
-                                    unzip=True)
-with zipfile.ZipFile('doqwnload.zip', 'r') as zip_ref:
+file_id = '1wMeJXGaFF1ku2-txWshzDLaHoxS_tBz0'
+dest_path = 'downloaded_file.zip'
+download_file_from_google_drive(file_id, dest_path)
+
+with zipfile.ZipFile('downloaded_file.zip.zip', 'r') as zip_ref:
     zip_ref.extractall()
 
 st.write(os.listdir())
