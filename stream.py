4303fb4c940e25b616b08feabceb7d45775fc844
Analyst-FPnA <analyst.fpna@gmail.com>
2024-07-31 16:02:45 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -7,7 +7,7 @@ import os
 
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
-gdd.download_file_from_google_drive(file_id='1f-YfvMvFG0UaOw9H5RqNpkPRJEB-8gfa',
+gdd.download_file_from_google_drive(file_id='1wMeJXGaFF1ku2-txWshzDLaHoxS_tBz0',
                                     dest_path='doqwnload.zip',
                                     unzip=True)
 with zipfile.ZipFile('doqwnload.zip', 'r') as zip_ref:
