735951f379387de9dad09076b417f22b39f69f54
Analyst-FPnA <analyst.fpna@gmail.com>
2024-07-31 15:25:28 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -9,12 +9,13 @@ url = 'https://drive.google.com/uc?export=download&id=1wMeJXGaFF1ku2-txWshzDLaHo
 response = requests.get(url)
 response.raise_for_status()
 
-# Menyimpan file untuk pemeriksaan manual
-with open('downloaded_file.zip', 'wb') as file:
-    file.write(response.content)
+
 
 # Memeriksa konten file yang diunduh
 with tempfile.TemporaryDirectory() as tmpdirname:
-    with zipfile.ZipFile('downloaded_file.zip', 'r') as zip_ref:
+    # Menyimpan file untuk pemeriksaan manual
+    with open(f'{tmpdirname}/downloaded_file.zip', 'wb') as file:
+        file.write(response.content)
+    with zipfile.ZipFile(f'{tmpdirname}/downloaded_file.zip', 'r') as zip_ref:
         zip_ref.extractall(tmpdirname)
     st.write(pd.read_csv(f'{tmpdirname}/Compile Breakdown (ALL).csv').head())
