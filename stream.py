408f30de4b90a09832e8764dedaa8df266c7aa99
Analyst-FPnA <analyst.fpna@gmail.com>
2024-07-31 15:28:58 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -17,7 +17,7 @@ st.write(os.listdir())
 
 # Memeriksa konten file yang diunduh
 with tempfile.TemporaryDirectory() as tmpdirname:
-
-    with zipfile.ZipFile(f'{tmpdirname}/downloaded_file.zip', 'r') as zip_ref:
+    with zipfile.ZipFile('downloaded_file.zip', 'r') as zip_ref:
         zip_ref.extractall(tmpdirname)
+    st.write(os.listdir(tmpdirname))
     st.write(pd.read_csv(f'{tmpdirname}/Compile Breakdown (ALL).csv').head())
