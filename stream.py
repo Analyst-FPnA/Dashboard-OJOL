7b173f78bf7f1270c63e62e97fce5c8b1be0a068
Analyst-FPnA <analyst.fpna@gmail.com>
2024-08-01 08:54:14 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -87,7 +87,9 @@ if st.button('Show'):
         df_merge3 = df_merge2[df_merge2['KAT'].isin(['QRIS ESB','QRIS TELKOM'])].groupby('SOURCE')[['NOM']].sum().reset_index()
         df_merge3['KAT']='QRIS TELKOM/ESB'
         
-
+        if df_merge3.empty:
+            df_merge3.loc[len(df_merge3)] = ['INVOICE',0,'QRIS TELKOM/ESB']
+            df_merge3.loc[len(df_merge3)] = ['WEB',0,'QRIS TELKOM/ESB']
             
         df_merge = pd.pivot(data=pd.concat([df_merge2[df_merge2['KAT'].isin(['GO RESTO','GRAB FOOD','QRIS SHOPEE','SHOPEEPAY'])],df_merge3]), 
                  index='SOURCE', columns='KAT', values='NOM')
