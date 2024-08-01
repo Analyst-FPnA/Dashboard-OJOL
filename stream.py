1fd8d780c6d7d8bb756b77fd62f801d7ac2271c4
Analyst-FPnA <analyst.fpna@gmail.com>
2024-08-01 11:46:51 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -51,20 +51,20 @@ df_prov = df_prov.loc[:265, ['Nama','Provinsi Alamat','Kota Alamat']]
 df_prov = df_prov.rename(columns={'Nama':'Nama Cabang','Provinsi Alamat':'Provinsi', 'Kota Alamat': 'Kota/Kabupaten'})
 list_cab = df_prov['Nama Cabang'].str.extract(r'\((.*?)\)')[0].values
 
-tmpdirname = 'tmp'
-def download_file_from_google_drive(file_id, dest_path):
-    if not os.path.exists(dest_path):
-        url = f"https://drive.google.com/uc?id={file_id}"
-        gdown.download(url, dest_path, quiet=False)
-        with zipfile.ZipFile(f'{tmpdirname}/downloaded_file.zip', 'r') as zip_ref:
-            zip_ref.extractall(tmpdirname)
+with tempfile.TemporaryDirectory() as tmpdirname:
+    def download_file_from_google_drive(file_id, dest_path):
+        if not os.path.exists(dest_path):
+            url = f"https://drive.google.com/uc?id={file_id}"
+            gdown.download(url, dest_path, quiet=False)
+            with zipfile.ZipFile(f'{tmpdirname}/downloaded_file.zip', 'r') as zip_ref:
+                zip_ref.extractall(tmpdirname)
 
 file_id = '1wMeJXGaFF1ku2-txWshzDLaHoxS_tBz0'
 dest_path = f'{tmpdirname}/downloaded_file.zip'
 download_file_from_google_drive(file_id, dest_path)
 
 st.title('Dashboard - Selisih Ojol')
-df_cab = pd.read_csv(f'{tmpdirname}/Compile Merge (ALL).csv')
+df_cab = pd.read_csv(f'tmp/{tmpdirname}/Compile Merge (ALL).csv')
 all_cab = st.multiselect('Pilih Cabang', df_cab['KAT'].unique())
 all_cab = list(all_cab)
 
@@ -77,8 +77,8 @@ bulan = st.selectbox('Pilih Bulan', all_bulan)
 
 
 if st.button('Show'):
-        df_merge = pd.read_csv(f'{tmpdirname}/Compile Merge (ALL).csv')
-        df_breakdown = pd.read_csv(f'{tmpdirname}/Compile Breakdown (ALL).csv')
+        df_merge = pd.read_csv(f'tmp/{tmpdirname}/Compile Merge (ALL).csv')
+        df_breakdown = pd.read_csv(f'tmp/{tmpdirname}/Compile Breakdown (ALL).csv')
         #st.write(df_merge.loc[0,'DATE'])
         #st.write(start_date)
         
