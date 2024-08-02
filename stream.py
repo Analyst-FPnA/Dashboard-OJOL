00003aab913d8563bd58911b962356bf5b332635
Analyst-FPnA <analyst.fpna@gmail.com>
2024-08-02 13:36:34 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -161,6 +161,8 @@ if (st.button("Show", on_click=callback) or st.session_state.button_clicked):
         df_breakdown['Kategori'] = df_breakdown['Kategori'].str.upper()
         st.write(df_breakdown.head())
         df_breakdown.columns = df_breakdown.columns[:-7].to_list() + ['GO RESTO','GRAB FOOD','QRIS SHOPEE','QRIS TELKOM/ESB','SHOPEEPAY'] + df_breakdown.columns[-2:].to_list()
+        df_breakdown.iloc[:,9:13] = df_breakdown.iloc[:,9:13].applymap(lambda x: str(x).replace(',', '')).astype('float')
+        
         st.write(df_breakdown.head())
         for cab in all_cab:
             df_merge2 = df_merge[df_merge['CAB'] == cab]
