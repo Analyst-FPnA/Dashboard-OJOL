b6c58caa8012fbba1c53be7ebd35e30037fd879f
Analyst-FPnA <analyst.fpna@gmail.com>
2024-08-23 15:51:56 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -16,24 +16,22 @@ import plotly.express as px
 import plotly.graph_objs as go
 
 st.set_page_config(layout="wide")
-def highlight_header(x):
+def highlight_header(df):
     """
     Meng-highlight header tabel dengan warna merah.
     
     Parameters:
-    - x: DataFrame yang akan diterapkan styling
+    - df: DataFrame yang akan diterapkan styling
     
     Returns:
     - DataFrame dengan styling untuk header
     """
-    # CSS styling untuk header
-    header_color = 'background-color: #FF4B4B; color: white;'  # Warna merah dengan teks putih
-    df_styles = pd.DataFrame('', index=x.index, columns=x.columns)
-    
-    # Memberikan warna khusus pada header
-    df_styles.loc[x.columns, :] = header_color
-
-    return df_styles
+    return df.style.set_table_styles(
+        [{'selector': 'thead th',
+          'props': [('background-color', '#FF4B4B'),
+                    ('color', 'white'),
+                    ('font-weight', 'bold')]}]
+    )
     
 if 'button_clicked' not in st.session_state:
     st.session_state.button_clicked = False
@@ -103,7 +101,7 @@ df_4101_1['Total']=df_4101_1.iloc[:,2:].sum(axis=1)
 pd.options.display.float_format = '{:,.0f}'.format
 df_4101_2 = df_4101.groupby(['Nama Cabang','Nomor #','Kode Barang','Nama Barang','Tipe Penyesuaian'])[['Kuantitas','Total Biaya']].sum().reset_index()
 df_4101_2 = df_4101_2.pivot(index=['Nama Cabang','Nomor #','Kode Barang','Nama Barang'],columns=['Tipe Penyesuaian'],values=['Kuantitas','Total Biaya']).reset_index().fillna('')
-st.dataframe(df_4101_1.style.apply(highlight_header, axis=None), use_container_width=True, hide_index=True)
+st.dataframe(highlight_header(df_4101_1), use_container_width=True, hide_index=True)
 
 all_month = []
 for i in month:
