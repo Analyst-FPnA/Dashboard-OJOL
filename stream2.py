5f2df99aff720470ddcc77869c17202ea11645e5
Analyst-FPnA <analyst.fpna@gmail.com>
2024-09-03 11:54:39 +0700

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -184,7 +184,7 @@ df_pic = df_selisih.merge(pic,how='left',left_on=['MONTH','CAB'],right_on =['BUL
 df_pic['MONTH'] = pd.Categorical(df_pic['MONTH'], categories=['January','February','March','April','May','June','July'], ordered=True)
 df_pic = df_pic.sort_values(['NAMA PIC','MONTH'])
 df_pic = df_pic.pivot(index='NAMA PIC',columns='MONTH',values='SELISIH').reset_index()
-df_pic.iloc[:,1:] = df_pic.iloc[:,1:].fillna(0).astype(float).applymap(lambda x: f'{x:,.0f}')
+df_pic.iloc[:,1:] = df_pic.iloc[:,1:].fillna(0).astype(float).applymap(lambda x: f'{x:.0f}')
 df_pic = df_pic.style.format(lambda x: '' if x==0 else x).background_gradient(cmap='Reds', axis=1, subset=df_pic.columns[1:])
 st.dataframe(df_pic, use_container_width=True, hide_index=True) 
 
