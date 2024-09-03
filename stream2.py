71d333e7605b9f7fc15cad1f199719c1bff4cad3
Analyst-FPnA <analyst.fpna@gmail.com>
2024-09-03 14:01:10 +0700

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -183,11 +183,11 @@ def format_number(x):
     return x
 
 
-df_pic = df_selisih.merge(pic,how='left',left_on=['MONTH','CAB'],right_on =['BULAN','NAMA RESTO']).groupby(['NAMA PIC','MONTH'])[['SELISIH']].sum().reset_index()
+df_pic = df_selisih.merge(pic,how='left',left_on=['MONTH','CAB'],right_on =['BULAN','NAMA RESTO']).groupby(['NAMA PIC','MONTH','CAB')[['SELISIH']].sum().reset_index()
 df_pic['MONTH'] = pd.Categorical(df_pic['MONTH'], categories=['January','February','March','April','May','June','July'], ordered=True)
 df_pic = df_pic.sort_values(['NAMA PIC','MONTH'])
-df_pic = df_pic.pivot(index='NAMA PIC',columns='MONTH',values='SELISIH').reset_index()
-#df_pic.iloc[:,1:] = df_pic.iloc[:,1:].applymap(lambda x: f'{x:.0f}')
+df_pic = df_pic.pivot(index=['NAMA PIC','CAB'],columns='MONTH',values='SELISIH').reset_index()
+#df_pic.iloc[:,2:] = df_pic.iloc[:,2:].applymap(lambda x: f'{x:.0f}')
 df_pic = df_pic.fillna(0).style.format(lambda x: format_number(x)).background_gradient(cmap='Reds', axis=1, subset=df_pic.columns[1:])
 st.dataframe(df_pic, use_container_width=True, hide_index=True) 
 
