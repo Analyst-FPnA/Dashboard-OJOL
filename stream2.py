86cf6d71c9dd5a2db399005472b41bbdf62f5c88
Analyst-FPnA <analyst.fpna@gmail.com>
2024-09-03 13:10:38 +0700

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -178,14 +178,17 @@ def highlight_last_row(x):
 def format_number(x):
     if isinstance(x, (int, float)):
         return "{:,.0f}".format(x)
+    if x==0:
+        return ''
     return x
-    
+
+
 df_pic = df_selisih.merge(pic,how='left',left_on=['MONTH','CAB'],right_on =['BULAN','NAMA RESTO']).groupby(['NAMA PIC','MONTH'])[['SELISIH']].sum().reset_index()
 df_pic['MONTH'] = pd.Categorical(df_pic['MONTH'], categories=['January','February','March','April','May','June','July'], ordered=True)
 df_pic = df_pic.sort_values(['NAMA PIC','MONTH'])
 df_pic = df_pic.pivot(index='NAMA PIC',columns='MONTH',values='SELISIH').reset_index()
 #df_pic.iloc[:,1:] = df_pic.iloc[:,1:].applymap(lambda x: f'{x:.0f}')
-df_pic = df_pic.fillna(0).style.format(lambda x: '' if x==0 else x).background_gradient(cmap='Reds', axis=1, subset=df_pic.columns[1:])
+df_pic = df_pic.fillna(0).style.format(lambda x: format_number(x)).background_gradient(cmap='Reds', axis=1, subset=df_pic.columns[1:])
 st.dataframe(df_pic, use_container_width=True, hide_index=True) 
 
 if 'All' in all_cab_selisih:
