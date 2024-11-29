8b0268e5c1bcd9c3d4e5c7fdb3e450bd522f2a5c
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-29 14:05:35 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -249,7 +249,7 @@ def highlight_cells(x, highlight_info=df_pic2.drop(columns=['CAB','NAMA PIC','SE
         if row_index in df_styles.index and col_name in df_styles.columns:
             
             # Berikan warna latar belakang kuning pada cell yang dipilih
-            df_styles.at[row_index, col_name] = f'attr:{x.at[row_index, col_name]} 🔴;'
+            df_styles.at[row_index, col_name] = f'attr:{x.at[row_index, col_name]};' + ' ❗'
 
     return df_styles
 
