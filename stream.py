00147e12305e2d9403f95d0ca000d8b5bb115c93
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-29 14:39:05 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -248,11 +248,11 @@ def highlight_cells(x, highlight_info=df_pic2.drop(columns=['CAB','NAMA PIC','SE
         # Memeriksa apakah row_index dan col_name ada di DataFrame
         if row_index in df_styles.index and col_name in df_styles.columns:
             # Berikan warna latar belakang kuning pada cell yang dipilih
-            df_styles.at[row_index, col_name] = "attr: x.at[row_index, col_name];". "\\1F534"
+            df_styles.at[row_index, col_name] = f"{x.at[row_index, col_name]} 🔴"
 
     return df_styles
 
 
-styled_pivot_df = df_pic.style.apply(highlight_cells, highlight_info=df_pic2.drop(columns=['CAB','NAMA PIC','SELISIH']), axis=None)
+styled_pivot_df = df_pic.apply(highlight_cells, highlight_info=df_pic2.drop(columns=['CAB','NAMA PIC','SELISIH']), axis=None)
 st.dataframe(styled_pivot_df.to_html(escape=False), use_container_width=True, hide_index=True) 
 
