5faa93afc14ae9db88c0274addc8743e487b981a
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-29 10:54:22 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -248,7 +248,7 @@ def highlight_cells(x, highlight_info=df_pic2.drop(columns=['CAB','NAMA PIC','SE
         # Memeriksa apakah row_index dan col_name ada di DataFrame
         if row_index in df_styles.index and col_name in df_styles.columns:
             df_styles.at[row_index, col_name] = 'background-color: yellow;'
-            x.at[row_index, col_name] = f"{x.at[row_index, col_name]} ❗"  # Menambahkan simbol exclamation merah.
+            df_styles.at[row_index, col_name] = f"{x.at[row_index, col_name]} ❗"  # Menambahkan simbol exclamation merah.
 
     return df_styles
 
