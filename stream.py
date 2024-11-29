5a2531b8541470dc5199b87f33b2a312e5a7d505
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-29 13:49:23 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -268,7 +268,7 @@ def apply_red_symbol(row):
 
 for idx, col in zip(df_pic2['index'], df_pic2['MONTH']):
     # Menambahkan simbol merah (🔴) pada nilai cell
-    df_pic.at[idx, col] = f'🔴 {df_pic.loc[idx, col]}'
+    df_pic.loc[idx, col] = f'🔴 {df_pic.loc[idx, col]}'
 
 df_pic
 #styled_pivot_df = df_pic.style.format(lambda x: format_number(x)).background_gradient(cmap='Reds', axis=1, subset=df_pic.columns[2:]).apply(apply_red_symbol, axis=1)
